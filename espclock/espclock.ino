a0f1ca1311c1432d076fa6f45e71890bcfd46284
ItsQuadrus <quadrus@gaboule.com>
2024-03-24 15:36:41 +0100

    heavy refactoring of loop

--- espclock/espclock.ino
+++ espclock/espclock.ino
@@ -33,7 +33,7 @@ WiFiServer server(80);
 String header;
 
 // Auxiliar variables to store the current output state
-String init_clock_done = "no";
+boolean initClockDone = false;
 String output4State = "off";
 
 // Current time
@@ -240,149 +240,182 @@ void ota_error(ota_error_t error)
   Serial.printf("Error[%u]: %s\n", error, ota_error_message(error));
 }
 
+/*
+ * HTTP request handling to interact with the clock
+ * TODO: make this more readable
+ */
+void handleHttpRequest(WifiClient &client)
+{                                // If a new client connects,
+  Serial.println("New Client."); // print a message out in the serial port
+  String currentLine = "";       // make a String to hold incoming data from the client
+  currentTime = millis();
+  previousTime = currentTime;
+  while (client.connected() && currentTime - previousTime <= timeoutTime)
+  { // loop while the client's connected
+    currentTime = millis();
+    if (client.available())
+    {                         // if there's bytes to read from the client,
+      char c = client.read(); // read a byte, then
+      Serial.write(c);        // print it out the serial monitor
+      header += c;
+      if (c == '\n')
+      { // if the byte is a newline character
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0)
+        {
+          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+          // and a content-type so the client knows what's coming, then a blank line:
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println("Connection: close");
+          client.println();
+
+          // turns the GPIOs on and off
+          if (header.indexOf("GET /set_time") >= 0)
+          {
+            if (init_clock_done == "no")
+            {
+              set_time_flag = true;
+            }
+          }
+          else if (header.indexOf("GET /adjust_1_min") >= 0)
+          {
+            increment_time(1);
+          }
+          else if (header.indexOf("GET /adjust_minus_1_min") >= 0)
+          {
+            increment_time(-1);
+          }
+
+          // Display the HTML web page
+          client.println("<!DOCTYPE html><html>");
+          client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+          client.println("<link rel=\"icon\" href=\"data:,\">");
+          // CSS to style the on/off buttons
+          // Feel free to change the background-color and font-size attributes to fit your preferences
+          client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+          client.println(".button {background-color: #195B6A; border: none; color: white; padding: 16px 40px; width: 350px; border-radius: 8px;");
+          client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+          client.println(".button2 {background-color: #195B6A; width: 350px; border-radius: 8px;}");
+          client.println(".button3 {background-color: #195B6A; width: 350px; border-radius: 8px;}");
+          client.println("</style></head>");
+
+          // Web Page Heading
+          client.println("<body><h1>keep clock at 12'O clock position and press SET TIME button to Auto calibrate</h1>");
+          if (init_clock_done == "yes")
+          {
+            client.println("<p>Clock INIT - completed</p>");
+          }
+          client.println("<p><a href=\"/set_time\"><button class=\"button\">SET TIME</button></a></p>");
+          client.println("<p><a href=\"/adjust_1_min\"><button class=\"button button2\">Increment 1 min</button></a></p>");
+          client.println("<p><a href=\"/adjust_minus_1_min\"><button class=\"button button3\">Decrement 1 min</button></a></p>");
+          // Display current state, and ON/OFF buttons for GPIO 5
+          // client.println("<p>GPIO 5 - State " + output5State + "</p>");
+          // If the output5State is off, it displays the ON button
+          // if (output5State=="off") {
+          //  client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+          //} else {
+          //  client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+          //}
+
+          // Display current state, and ON/OFF buttons for GPIO 4
+          //            client.println("<p>GPIO 4 - State " + output4State + "</p>");
+          //            // If the output4State is off, it displays the ON button
+          //            if (output4State=="off") {
+          //              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+          //            } else {
+          //              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+          //            }
+          client.println("</body></html>");
+
+          // The HTTP response ends with another blank line
+          client.println();
+          break;
+        }
+        else
+        { // if you got a newline, then clear currentLine
+          currentLine = "";
+        }
+      }
+      else if (c != '\r')
+      {                   // if you got anything else but a carriage return character,
+        currentLine += c; // add it to the end of the currentLine
+      }
+    }
+  }
+  // Clear the header variable
+  header = "";
+  // Close the connection
+  client.stop();
+  Serial.println("Client disconnected.");
+  Serial.println("");
+}
+
+void blink()
+{
+  if (currentTime - previousMillis >= interval)
+  {
+    digitalWrite(led, ledState);
+    ledState = !ledState; // toggle
+    previousMillis = currentTime;
+  }
+}
+
+void updateCurrentMinute()
+{
+  static long prev_min = 0, prev_pos = 0; // TODO: these variables seem unnecessary...?
+  long min = updateCurrentMinuteHelper();
+  long pos = calculatePosForMinute(min);
+  approach();
+
+  if (pos - prev_pos > 0)
+  {
+    rotate(pos - prev_pos);
+  }
+
+  prev_pos = pos;
+}
+
+long updateCurrentMinuteHelper()
+{
+  static long prev_min = 0;
+  long min = millis() / MILLIS_PER_MIN;
+
+  if (prev_min == min)
+  {
+    return prev_min;
+  }
+
+  prev_min = min;
+  return min;
+}
+
+long calculatePosForMinute(long min)
+{
+  return (STEPS_PER_ROTATION * min) / 60;
+}
+
+/*
+ * Main loop
+ */
 void loop()
 {
   ArduinoOTA.handle();
   WiFiClient client = server.available(); // Listen for incoming clients
 
-  if ((set_time_flag) && (init_clock_done == "no"))
+  if (set_time_flag && !init_clock_done)
   {
     update_ntp_time();
-    init_clock_done = "yes";
+    init_clock_done = true;
     set_time_flag = false;
   }
 
   if (client)
-  {                                // If a new client connects,
-    Serial.println("New Client."); // print a message out in the serial port
-    String currentLine = "";       // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime)
-    { // loop while the client's connected
-      currentTime = millis();
-      if (client.available())
-      {                         // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        Serial.write(c);        // print it out the serial monitor
-        header += c;
-        if (c == '\n')
-        { // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0)
-          {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /set_time") >= 0)
-            {
-              if (init_clock_done == "no")
-              {
-                set_time_flag = true;
-              }
-            }
-            else if (header.indexOf("GET /adjust_1_min") >= 0)
-            {
-              increment_time(1);
-            }
-            else if (header.indexOf("GET /adjust_minus_1_min") >= 0)
-            {
-              increment_time(-1);
-            }
-
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button {background-color: #195B6A; border: none; color: white; padding: 16px 40px; width: 350px; border-radius: 8px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #195B6A; width: 350px; border-radius: 8px;}");
-            client.println(".button3 {background-color: #195B6A; width: 350px; border-radius: 8px;}");
-            client.println("</style></head>");
-
-            // Web Page Heading
-            client.println("<body><h1>keep clock at 12'O clock position and press SET TIME button to Auto calibrate</h1>");
-            if (init_clock_done == "yes")
-            {
-              client.println("<p>Clock INIT - completed</p>");
-            }
-            client.println("<p><a href=\"/set_time\"><button class=\"button\">SET TIME</button></a></p>");
-            client.println("<p><a href=\"/adjust_1_min\"><button class=\"button button2\">Increment 1 min</button></a></p>");
-            client.println("<p><a href=\"/adjust_minus_1_min\"><button class=\"button button3\">Decrement 1 min</button></a></p>");
-            // Display current state, and ON/OFF buttons for GPIO 5
-            // client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button
-            // if (output5State=="off") {
-            //  client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            //} else {
-            //  client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            //}
-
-            // Display current state, and ON/OFF buttons for GPIO 4
-            //            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            //            // If the output4State is off, it displays the ON button
-            //            if (output4State=="off") {
-            //              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            //            } else {
-            //              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            //            }
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-
-  // loop to blink without delay
-  unsigned long currentMillis = millis();
-  //  if (currentMillis - previousMillis >= interval) {
-  //    digitalWrite(led,  ledState);
-  //  }
-
-  static long prev_min = 0, prev_pos = 0;
-  long min;
-  static long pos;
-
-  min = millis() / MILLIS_PER_MIN;
-  if (prev_min == min)
   {
-    return;
+    handleHttpRequest(client);
   }
-  prev_min = min;
-  pos = (STEPS_PER_ROTATION * min) / 60;
-  rotate(-20); // for approach run
-  rotate(20);  // approach run without heavy load
-  if (pos - prev_pos > 0)
-  {
-    rotate(pos - prev_pos);
-  }
-  prev_pos = pos;
+
+  blink();
+  updateCurrentMinute();
 }
